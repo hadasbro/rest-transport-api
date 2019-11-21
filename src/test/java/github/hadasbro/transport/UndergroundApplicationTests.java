c3b2161b237f447323c2f489658f90c57e370e85
Slawek <slawomir.hadas@yahoo.com>
2019-11-21 00:10:41 +0000

    readme

--- src/test/java/github/hadasbro/transport/UndergroundApplicationTests.java
+++ src/test/java/github/hadasbro/transport/UndergroundApplicationTests.java
@@ -1,6 +1,7 @@
 package github.hadasbro.transport;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UndergroundApplicationTests {
-
+    @Test
+    public void test(){
+        assert true;
+    }
 }
